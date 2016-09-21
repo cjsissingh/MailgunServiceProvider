2ea02649849c9b9f5eb7034049ee0bb6889a65ce
Chris Sissingh <chris@thisistell.com>
2016-09-21 16:02:02 +1200

    Updated to Silex 2.0

--- src/Provider/MailgunServiceProvider.php
+++ src/Provider/MailgunServiceProvider.php
@@ -5,17 +5,18 @@
  *
  * A Simple wrapper for the mailgun API for the Silex Framework
  *
- * @package		MailgunServiceProvider
- * @author		Achraf Soltani <achraf.soltani@gmail.com>
+ * @package     MailgunServiceProvider
+ * @author      Achraf Soltani <achraf.soltani@gmail.com>
  * @date        05/19/2015
  * @file        MailgunServiceProvider.php
  */
- 
+
 
 namespace AchrafSoltani\Provider;
 
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
 use AchrafSoltani\Mailgun\Client;
 
 /**
@@ -24,19 +25,18 @@ use AchrafSoltani\Mailgun\Client;
  */
 class MailgunServiceProvider implements ServiceProviderInterface
 {
-    public function register(Application $app)
+    public function register(Container $app)
     {
-        
-        $app['mailgun'] = function () use ($app) 
-        {
-            $client = new Client($app['mailgun.api_key']); 
+
+        $app['mailgun'] = function () use ($app) {
+            $client = new Client($app['mailgun.api_key']);
             $client->setWorkingDomain($app['mailgun.domain']);
             return $client;
         };
     }
-    
+
     public function boot(Application $app)
     {
-        
+
     }
 }
