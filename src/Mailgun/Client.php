5973ae6c2411ab6517add83ff013ef929c6cbe4e
Chris Sissingh <chris@thisistell.com>
2016-09-21 16:03:34 +1200

    PHP linting. Fixing whitespace errors

--- src/Mailgun/Client.php
+++ src/Mailgun/Client.php
@@ -5,36 +5,35 @@
  *
  * A class wrapping the Mailgun SDK class
  *
- * @package		MailgunServiceProvider
- * @author		Achraf Soltani <achraf.soltani@gmail.com>
+ * @package     MailgunServiceProvider
+ * @author      Achraf Soltani <achraf.soltani@gmail.com>
  * @date        05/20/2015
  * @file        Client.php
  */
- 
+
  namespace AchrafSoltani\Mailgun;
- 
+
  use Mailgun\Mailgun;
- 
- class Client extends Mailgun
- {
-     protected $workingDomain;
-     
-     public function setWorkingDomain($workingDomain)
-     {
-         $this->workingDomain = $workingDomain;
-     }
-     
-     /**
-     * This method wraps the original sendMessage method while avoiding
-     * the need to specify the working domain param
-     *
-     * @param array $postData
-     * @param array $postFiles
-     * @throws Exceptions\MissingRequiredMIMEParameters
-     */
-     public function sendMessage($postData, $postFiles = array())
-     {
-         return parent::sendMessage($this->workingDomain, $postData, $postFiles);
-     }
-         
- }
+
+class Client extends Mailgun
+{
+    protected $workingDomain;
+
+    public function setWorkingDomain($workingDomain)
+    {
+        $this->workingDomain = $workingDomain;
+    }
+
+    /**
+    * This method wraps the original sendMessage method while avoiding
+    * the need to specify the working domain param
+    *
+    * @param array $postData
+    * @param array $postFiles
+    * @throws Exceptions\MissingRequiredMIMEParameters
+    */
+    public function sendMessage($postData, $postFiles = array())
+    {
+        return parent::sendMessage($this->workingDomain, $postData, $postFiles);
+    }
+}
